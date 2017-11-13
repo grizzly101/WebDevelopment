cdd651c19a920f4671b90a68bf9f6255e9c66e23
jblackford3 <jason.blackford@gtri.gatech.edu>
2017-11-13 15:05:51 -0500

    Added remaining code from MxGraph HelloWorld.html to React script. Required binding objects correctly to functors.

--- BrowserUserInterface/UI_Prototype.js
+++ BrowserUserInterface/UI_Prototype.js
@@ -1,16 +1,4 @@
 
-class Canvas extends React.Component {
-  constructor(){
-    super();
-
- 
-  };
- 
-  render() {
-    
-  }
-  
-}
 
 class SelectMenu extends React.Component {
   render() {
@@ -32,25 +20,41 @@ class SelectMenu extends React.Component {
 }
 
 
-    
+
+
+ 	
 class EntireUserInterface extends React.Component {
   
   constructor(props){
     super(props);
     
-    this.graph = new mxGraph(container)
+    this.graph = new mxGraph(container);
+   
+      /**
+				 * Returns the padding for pages in page view with scrollbars.
+				 */
+			
+    
        this.main(document.getElementById('graphContainer'));
+
   }
+  
   
   render() {
     return (
       <div>
-        <Canvas />
+      
   
         <SelectMenu />
       </div>
     );
   }
+  
+   /**
+				 * Returns the padding for pages in page view with scrollbars.
+				 */
+		
+  
   
   	main(container)
 		{
@@ -68,6 +72,170 @@ class EntireUserInterface extends React.Component {
 				// Creates the graph inside the given container
 				this.graph = new mxGraph(container);
 
+        // If the mouse is over a cell object (node or edge) disable panning
+				this.graph.panningHandler.ignoreCell = false;
+				this.graph.setPanning(true);
+        
+        /**
+				 * Specifies the size of the size for "tiles" to be used for a graph with
+				 * scrollbars but no visible background page. A good value is large
+				 * enough to reduce the number of repaints that is caused for auto-
+				 * translation, which depends on this value, and small enough to give
+				 * a small empty buffer around the graph. Default is 400x400.
+				 */
+				this.graph.scrollTileSize = new mxRectangle(0, 0,10, 10);
+        
+       
+        
+        /**
+				 * Returns the size of the page format scaled with the page size.
+				 */
+				this.graph.getPageSize = function()
+				{
+					return (this.pageVisible) ? new mxRectangle(0, 0, this.pageFormat.width * this.pageScale,
+							this.pageFormat.height * this.pageScale) : this.scrollTileSize;
+				};
+        
+     /**
+				 * Returns the padding for pages in page view with scrollbars.
+				 */
+				this.graph.getPagePadding = function()
+				{
+					return new mxPoint(Math.max(0, Math.round(container.offsetWidth - 34)),
+							Math.max(0, Math.round(container.offsetHeight - 34)));
+				};
+        /**
+				 * Returns a rectangle describing the position and count of the
+				 * background pages, where x and y are the position of the top,
+				 * left page and width and height are the vertical and horizontal
+				 * page count.
+				 */
+				this.graph.getPageLayout = function()
+				{
+					var size = (this.pageVisible) ? this.getPageSize() : this.scrollTileSize;
+					var bounds = this.getGraphBounds();
+
+					if (bounds.width == 0 || bounds.height == 0)
+					{
+						return new mxRectangle(0, 0, 1, 1);
+					}
+					else
+					{
+						// Computes untransformed graph bounds
+						var x = Math.ceil(bounds.x / this.view.scale - this.view.translate.x);
+						var y = Math.ceil(bounds.y / this.view.scale - this.view.translate.y);
+						var w = Math.floor(bounds.width / this.view.scale);
+						var h = Math.floor(bounds.height / this.view.scale);
+						
+						var x0 = Math.floor(x / size.width);
+						var y0 = Math.floor(y / size.height);
+						var w0 = Math.ceil((x + w) / size.width) - x0;
+						var h0 = Math.ceil((y + h) / size.height) - y0;
+						
+						return new mxRectangle(x0, y0, w0, h0);
+					}
+				};
+        
+        
+        // Fits the number of background pages to the graph
+				this.graph.view.getBackgroundPageBounds = function()
+				{
+					var layout = this.graph.getPageLayout();
+					var page = this.graph.getPageSize();
+					
+					return new mxRectangle(this.scale * (this.translate.x + layout.x * page.width),
+							this.scale * (this.translate.y + layout.y * page.height),
+							this.scale * layout.width * page.width,
+							this.scale * layout.height * page.height);
+				};
+				
+				this.graph.getPreferredPageSize = function(bounds, width, height)
+				{
+					var pages = this.getPageLayout();
+					var size = this.getPageSize();
+					
+					return new mxRectangle(0, 0, pages.width * size.width, pages.height * size.height);
+				};
+				
+       
+        /**
+				 * Guesses autoTranslate to avoid another repaint (see below).
+				 * Works if only the scale of the graph changes or if pages
+				 * are visible and the visible pages do not change.
+				 */
+				var graphViewValidate = this.graph.view.validate;
+				this.graph.view.validate = function()
+				{
+					if (container != null && mxUtils.hasScrollbars(container))
+					{
+						  var pad = this.graph.getPagePadding();
+						var size = this.graph.getPageSize();
+						
+						// Updating scrollbars here causes flickering in quirks and is not needed
+						// if zoom method is always used to set the current scale on the graph.
+			
+						var tx = this.translate.x;
+						var ty = this.translate.y;
+						this.translate.x = pad.x / this.scale - (this.x0 || 0) * size.width;
+						this.translate.y = pad.y / this.scale - (this.y0 || 0) * size.height;
+					}
+					
+					graphViewValidate.apply(this, arguments);
+				};
+        	
+        var graphSizeDidChange = this.graph.sizeDidChange;
+        
+				this.graph.sizeDidChange = function()
+				{
+					
+							if (this.container != null && mxUtils.hasScrollbars(this.container))
+					{
+						var pages = this.getPageLayout();
+						var pad = this.getPagePadding();
+						var size = this.getPageSize();
+						
+						// Updates the minimum graph size
+						var minw = Math.ceil(2 * pad.x / this.view.scale + pages.width * size.width);
+						var minh = Math.ceil(2 * pad.y / this.view.scale + pages.height * size.height);
+						
+						var min = this.minimumGraphSize;
+						
+						// LATER: Fix flicker of scrollbar size in IE quirks mode
+						// after delayed call in window.resize event handler
+						if (min == null || min.width != minw || min.height != minh)
+						{
+						 this.minimumGraphSize = new mxRectangle(0, 0, minw, minh);
+						}
+						
+						// Updates auto-translate to include padding and graph size
+						var dx = pad.x / this.view.scale - pages.x * size.width;
+						var dy = pad.y / this.view.scale - pages.y * size.height;
+						
+						if (!this.autoTranslate && (this.view.translate.x != dx || this.view.translate.y != dy))
+						{
+							this.autoTranslate = true;
+							this.view.x0 = pages.x;
+							this.view.y0 = pages.y;
+
+							// NOTE: THIS INVOKES THIS METHOD AGAIN. UNFORTUNATELY THERE IS NO WAY AROUND THIS SINCE THE
+							// BOUNDS ARE KNOWN AFTER THE VALIDATION AND SETTING THE TRANSLATE TRIGGERS A REVALIDATION.
+							// SHOULD MOVE TRANSLATE/SCALE TO VIEW.
+							var tx = this.view.translate.x;
+							var ty = this.view.translate.y;
+
+							this.view.setTranslate(dx, dy);
+							this.container.scrollLeft += (dx - tx) * this.view.scale;
+							this.container.scrollTop += (dy - ty) * this.view.scale;
+
+							this.autoTranslate = false;
+							return;
+						}
+
+						graphSizeDidChange.apply(this, arguments);
+					}
+						
+				}
+        
 				// Enables rubberband selection
 				new mxRubberband(this.graph);
 				
@@ -89,7 +257,18 @@ class EntireUserInterface extends React.Component {
 					this.graph.getModel().endUpdate();
 				}
 			}
-		}
+      
+      /* Use bind(this) to bind the main EntireUserInterface object with the new object function or functor, which is being passed to setTimeout, allowing the 'this' keyword to refer to the EntireUserInterface object.*/
+      window.setTimeout(function()
+				{
+          
+					var bounds = this.graph.getGraphBounds();
+					var width = Math.max(bounds.width, this.graph.scrollTileSize.width * this.graph.view.scale);
+					var height = Math.max(bounds.height, this.graph.scrollTileSize.height * this.graph.view.scale);
+					this.graph.container.scrollTop = Math.floor(Math.max(0, bounds.y - Math.max(20, (this.graph.container.clientHeight - height) / 4)));
+					this.graph.container.scrollLeft = Math.floor(Math.max(0, bounds.x - Math.max(0, (this.graph.container.clientWidth - width) / 2)));
+				}.bind(this), 0);
+		}//main
   
 }
 
